11064787ab82ee6a2c42a66cdf5efe0fcef05c28
rbacordio <113413337+rbacordio@users.noreply.github.com>
2022-09-13 14:12:03 +0800

    Create Item Model With Entity Framework Support

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using WishList.Data;
 
 namespace WishList
 {
@@ -16,8 +17,10 @@ namespace WishList
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc(); 
-
+            services.AddMvc();
+            services.AddDbContext<ApplicationDbContext>(options => {
+                options.UseInMemoryDatabase("WishList");
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
